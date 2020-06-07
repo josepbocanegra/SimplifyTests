97b267c7799fc9f6ed5ce7899995ecabd2c6dbcf
josep-bocanegra <40354250+josep-bocanegra@users.noreply.github.com>
2020-06-07 18:38:34 +0200

    vipcustomers list in alphabetical order

---
+++ SimplifyTests.Tests/VipCustomersShould.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using Xunit;
+
+namespace SimplifyTests.Tests
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class VipCustomersShould
+    {
+        [Fact]
+        public void ReturnCustomersFullNameInAlphabeticalOrder()
+        {
+            var vipCustomers = new VipCustomers();
+            vipCustomers.Add(new Customer("Mandy", "Harris"));
+            vipCustomers.Add(new Customer("John", "Smith"));
+            vipCustomers.Add(new Customer("Albert", "Thomson"));
+
+            var orderedVipCustomers = vipCustomers.InAlphabeticalOrder();
+
+            Approvals.VerifyAll(orderedVipCustomers, "");
+        }
+
+    }
+}
---
+++ SimplifyTests/VipCustomers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimplifyTests
+{
+    public class VipCustomers
+    {
+        private readonly List<Customer> vipCustomers;
+
+        public VipCustomers()
+        {
+            vipCustomers = new List<Customer>();
+        }
+
+        public void Add(Customer customer)
+        {
+            vipCustomers.Add(customer);
+        }
+
+        public List<string> InAlphabeticalOrder()
+        {
+            return vipCustomers.Select(c => c.FullName()).ToList();
+        }
+    }
+}
