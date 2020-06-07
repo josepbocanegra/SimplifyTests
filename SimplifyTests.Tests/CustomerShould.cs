d8f11949393363d5cf761d92cce8a17e3325164b
josep-bocanegra <40354250+josep-bocanegra@users.noreply.github.com>
2020-06-07 17:57:25 +0200

    test customer's fullname

---
+++ SimplifyTests.Tests/CustomerShould.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SimplifyTests.Tests
+{
+    public class CustomerShould
+    {
+        [Fact]
+        public void ShowFullName()
+        {
+            const string expectedFullName = "John Smith";
+            var customer = new Customer("John", "Smith");
+            
+            var fullName = customer.FullName();
+            
+            Assert.Equal(expectedFullName, fullName);
+        }
+    }
+}
