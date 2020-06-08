ad4b2e7d59552d5d510fc6c6b3bf52893d8ff5b2
Josep Bocanegra <josepboc@hotmail.com>
2020-06-08 15:30:24 +0200

    use approvals with theory

--- SimplifyTests.Tests/CustomerShould.cs
+++ SimplifyTests.Tests/CustomerShould.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using ApprovalTests;
+using ApprovalTests.Namers;
 using ApprovalTests.Reporters;
 using Xunit;
 
@@ -18,6 +19,21 @@ namespace SimplifyTests.Tests
             
             var fullName = customer.FullName();
             
+            Approvals.Verify(fullName);
+        }
+
+        //Another version of the same test but using Theory
+        [Theory]
+        [InlineData("Albert", "Thomson")]
+        [InlineData("Mandy", "Harris")]
+        [InlineData("John", "Smith")]
+        public void ShowFullName2(string firstName, string lastName)
+        {
+            NamerFactory.AdditionalInformation = $"-{firstName}{lastName}";
+            var customer = new Customer(firstName, lastName);
+
+            var fullName = customer.FullName();
+
             Approvals.Verify(fullName);
         }
     }
